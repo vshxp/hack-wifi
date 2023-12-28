77903b5b62e64c574322a9da87ea42d06074a1aa
vshxp <matheus@matfor.com.br>
2023-12-28 18:36:48 -0300

    fix: lint

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-
+import argparse
 import socket
 import wifi
-import argparse
 
 
 def get_wifi_info(interface):
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sockfd = sock.fileno()
-        iw_cells = wifi.Cell.all(interface)
-        wifi_info = [(cell.ssid, cell.address) for cell in iw_cells]
-        return wifi_info
-    except FileNotFoundError:
-        print("Error: Unable to get WiFi information. Please check your interface name and make sure it is correct.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+  try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sockfd = sock.fileno()
+    iw_cells = wifi.Cell.all(interface)
+    wifi_info = [(cell.ssid, cell.address) for cell in iw_cells]
+    return wifi_info
+  except FileNotFoundError:
+    print("Error: Unable to get WiFi information. Check if your interface name is correct.")
+  except Exception as e:
+    print(f"An error occurred: {e}")
 
 
 def is_vuln(ssid: str) -> bool:
-	if 'VIVOFIBRA-' in ssid or 'CLARO' in ssid or 'VIVO-' in ssid: 
+	if 'VIVOFIBRA-' in ssid or 'CLARO' in ssid or 'VIVO-' in ssid:
 		return True
-	else: 
+	else:
 		return False
 
 
@@ -32,26 +31,28 @@ def own_vivo(mac: str) -> str:
 	return mac.replace(":","").upper()[2:]
 
 
-def own_claro(mac: str) -> str :
+def own_claro(mac: str) -> str:
 	return mac.replace(":","").upper()[4:]
 
 
 def wifi_owner(wifi_list: list):
-    if wifi_list:
-        print("Available WiFi Information:")
-        for ssid, mac_address in wifi_list:
-            if is_vuln(ssid):
-                if 'VIVOFIBRA-' in ssid:
-                    print(f"  Password: {own_vivofibra(ssid=ssid, mac=mac_address)} \t SSID: {ssid}")
-                if 'VIVO-' in ssid:
-                    print(f"  Password: {own_vivo(mac=mac_address)} \t SSID: {ssid}")
-                if 'CLARO-' in ssid:
-                    print(f"  Password: {own_claro(ssid=ssid, mac=mac_address)} \t SSID: {ssid}")
+  if wifi_list:
+    print("Available WiFi Information:")
+    for ssid, mac_address in wifi_list:
+      if is_vuln(ssid):
+        if 'VIVOFIBRA-' in ssid:
+          print(f"  Password: {own_vivofibra(ssid=ssid, mac=mac_address)} \t SSID: {ssid}")
+        if 'VIVO-' in ssid:
+          print(f"  Password: {own_vivo(mac=mac_address)} \t SSID: {ssid}")
+        if 'CLARO-' in ssid:
+          print(f"  Password: {own_claro(ssid=ssid, mac=mac_address)} \t SSID: {ssid}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--interface", default="wlp0s20f3", help="Specify the network interface (default: wlp0s20f3)")
+    parser.add_argument("-i", "--interface", 
+                        default="wlp0s20f3", 
+                        help="Specify the network interface (default: wlp0s20f3)")
     
     args = parser.parse_args()
     wifi_owner(get_wifi_info(args.interface))
