9c8cf6c93b662f9bdf5ebeef9ed0f68e20121051
vshxp <matheus@matfor.com.br>
2023-12-28 19:00:40 -0300

    fix: lint 6

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import socket
 import wifi
 
 
-
 def get_wifi_info(interface) -> list:
     """
     Return a list with ssid and mac addresses
@@ -15,46 +14,45 @@ def get_wifi_info(interface) -> list:
         iw_cells = wifi.Cell.all(interface)
         wifi_info = [(cell.ssid, cell.address) for cell in iw_cells]
         return wifi_info
-    except FileNotFoundError:
-        print("Error: Unable to get WiFi information. Check if your interface name is correct.")
     except Exception as e:
         print(f"An error occurred: {e}")
+        return None
 
 
-"""
-Check if the ssid contains the default name pattern
-"""
 def is_vuln(ssid: str) -> bool:
+    """
+    Check if the ssid contains the default name pattern
+    """
     if 'VIVOFIBRA-' in ssid or 'CLARO' in ssid or 'VIVO-' in ssid:
         return True
     return False
 
 
-"""
-Own vivo fibra default password
-"""
 def own_vivofibra(ssid: str,mac:str) -> str:
+    """
+    Own vivo fibra default password
+    """
     return mac.replace(":","").lower()[2:8] + ssid.split("-")[1].lower()
 
 
-"""
-Own vivo default password
-"""
 def own_vivo(mac: str) -> str:
+    """
+    Own vivo default password
+    """
     return mac.replace(":","").upper()[2:]
 
 
-"""
-Own claro default password
-"""
 def own_claro(mac: str) -> str:
+    """
+    Own claro default password
+    """
     return mac.replace(":","").upper()[4:]
 
 
-"""
-Function that check which own algorithm to use
-"""
 def wifi_owner(wifi_list: list):
+    """
+    Checks which own algorithm to use
+    """
     if wifi_list:
         print("Available WiFi Information:")
         for ssid, mac_address in wifi_list:
